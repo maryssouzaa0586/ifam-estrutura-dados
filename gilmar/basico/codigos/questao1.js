8d6af1992096c4c6df40ccfb948a5350de0a8759
maryssouza0586 <ms3451695@gmail.com>
2025-11-14 22:23:40 -0300

    algoritmo estrutura de dados

--- gilmar/basico/codigos/questao1.js
+++
@@ -1,8 +0,0 @@
-
-function tabuada(numero){
-    for (let i = 1; i<=10; i++){
-    console.log (`${numero}*${i} = ${numero * i}`);
-
-    }
-}
-let numero = prompt("informe um numero")
--- gilmar/ds/loop/fatorial.js
+++
@@ -1,6 +0,0 @@
-let numero = parseInt(prompt("Número ? "))
-var fatorial = 1
-for(let i=1; i<=numero; i++){
-   fatorial*=i;
-}
-document.writeln(`${numero} != ${fatorial}`)
